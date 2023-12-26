2af335d8d5a4738af690989e98da68266eb58cfc
Eiji MATSUMOTO <e.mattsan@gmail.com>
2023-12-26 19:11:50 +0900

    initial commit

---
+++ lib/bdf_parser.ex
@@ -0,0 +1,18 @@
+defmodule BDFParser do
+  @moduledoc """
+  Documentation for `BDFParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BDFParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bdf_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule BDFParserTest do
+  use ExUnit.Case
+  doctest BDFParser
+
+  test "greets the world" do
+    assert BDFParser.hello() == :world
+  end
+end
