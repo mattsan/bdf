efbc744a8cb13a804e70fff436c29979a485d896
MATSUMOTO Eiji <e.mattsan@gmail.com>
2024-01-20 22:41:17 +0900

    change tareget version (#4)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BDF.MixProject do
     [
       app: :bdf,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs()
